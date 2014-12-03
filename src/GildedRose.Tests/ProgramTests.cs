ce6584fbd39bc9e4dc2d3fc87f776df5a7903ce7
Andrew Frank <andrew.frank@wiredwithpurpose.com>
2014-12-03 13:50:35 -0500

    Adding initial spec for the UpdateQuality method.

---
+++ src/GildedRose.Tests/ProgramTests.cs
@@ -0,0 +1,34 @@
+﻿using GildedRose.Console;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GildedRose.Tests
+{
+  [TestFixture]
+  class ProgramTests
+  {
+    protected Program _program;
+
+    [SetUp]
+    public void SetUp()
+    {
+      _program = new Program();
+    }
+
+    protected class UpdateQualityTests : ProgramTests
+    {
+      [Test]
+      public void TestExpiredItemDegradesTwiceAsFast()
+      {
+        var item = new Item { SellIn = -1, Quality = 2 };
+        var items = new List<Item> { item };
+
+        Program.UpdateQuality(items);
+        Assert.AreEqual(0, item.Quality);
+      }
+    }
+  }
+}
